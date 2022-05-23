7f19b1e2662c8182cbe20bc8def097262083fd64
PoteznyDominator <b.murawski00@gmail.com>
2022-05-23 22:20:44 +0200

    Added functionality to open file

---
+++ src/Utility.h
@@ -0,0 +1,18 @@
+//
+// Created by PoteznyDominator on 23.05.2022.
+//
+
+#ifndef QEDITIOR_UTILITY_H
+#define QEDITIOR_UTILITY_H
+
+#include <QString>
+#include <QTabWidget>
+
+namespace Utils {
+  QString getFileName(const QString& filePath);
+
+  int getIndexOfOpenedFile(const QString& filePath, QTabWidget* tabWidget);
+}
+
+
+#endif//QEDITIOR_UTILITY_H
